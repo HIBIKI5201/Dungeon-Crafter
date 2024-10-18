85a8c52d0ec01aa515151dcab60a7840a9d8155a
HIBIKI_5201 <takuto5201@gmail.com>
2024-10-19 03:10:07 +0900

    [update]状態異常のEnumとリストを追加

--- Assets/Script/InGame/Enemy/EnemyManager_B.cs
+++ Assets/Script/InGame/Enemy/EnemyManager_B.cs
@@ -1,74 +1,95 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class EnemyManager_B : MonoBehaviour
+namespace DCFrameWork.Enemy
 {
-    [SerializeField]
-    protected float _maxHealth;
-    protected float _currentHealth;
-
-    [SerializeField]
-    protected float _defense;
-
-    [SerializeField]
-    protected float _dexterity;
-
-    [SerializeField]
-    protected float _specialChance;
-
-    [SerializeField]
-    protected float _plunder;
-
-    [SerializeField]
-    protected float _dropEXP;
-
-    [SerializeField]
-    protected float _dropGold;
-
-    private void Start()
+    public abstract class EnemyManager_B : MonoBehaviour
     {
-        _currentHealth = _maxHealth;
-    }
+        [SerializeField]
+        protected float _maxHealth;
+        protected float _currentHealth;
 
-    /// <summary>
-    /// サブクラスでのStartメソッド
-    /// </summary>
-    protected virtual void Start_S() { }
+        [SerializeField]
+        protected float _defense;
 
-    /// <summary>
-    /// ダメージを受ける
-    /// </summary>
-    /// <param name="damage">ダメージ量</param>
-    public void HitDamage(float damage)
-    {
-        _currentHealth -= damage;
-        if (_currentHealth <= 0)
+        [SerializeField]
+        protected float _dexterity;
+
+        [SerializeField]
+        protected float _specialChance;
+
+        [SerializeField]
+        protected float _plunder;
+
+        [SerializeField]
+        protected float _dropEXP;
+
+        [SerializeField]
+        protected float _dropGold;
+
+        private List<ConditionType> _conditionList = new();
+
+        private void Start()
         {
-            DeathBehivour();
+            _currentHealth = _maxHealth;
+        }
+
+        /// <summary>
+        /// サブクラスでのStartメソッド
+        /// </summary>
+        protected virtual void Start_S() { }
+
+        /// <summary>
+        /// ダメージを受ける
+        /// </summary>
+        /// <param name="damage">ダメージ量</param>
+        public void HitDamage(float damage)
+        {
+            _currentHealth -= damage;
+            if (_currentHealth <= 0)
+            {
+                DeathBehivour();
+            }
+        }
+
+        /// <summary>
+        /// 回復を受ける
+        /// </summary>
+        /// <param name="amount">回復量</param>
+        public void HitHeal(float amount)
+        {
+            _currentHealth = Mathf.Min(_currentHealth + amount, _maxHealth);
+        }
+
+        private void DeathBehivour()
+        {
+            Destroy(gameObject);
+        }
+
+        public void AddCondition(ConditionType type)
+        {
+            if (!_conditionList.Contains(type))
+                _conditionList.Add(type);
+        }
+
+        public void RemoveCondition(ConditionType type)
+        {
+            if (_conditionList.Contains(type))
+                _conditionList.Remove(type);
+        }
+
+        /// <summary>
+        /// NavMesh上のポジションへ移動する
+        /// </summary>
+        /// <param name="pos">移動目標の座標</param>
+        protected void GoToTargetPos(Vector3 pos)
+        {
+
         }
     }
-
-    /// <summary>
-    /// 回復を受ける
-    /// </summary>
-    /// <param name="amount">回復量</param>
-    public void HitHeal(float amount)
+    public enum ConditionType
     {
-        _currentHealth = Mathf.Min(_currentHealth + amount, _maxHealth);
-    }
-
-    private void DeathBehivour()
-    {
-        Destroy(gameObject);
-    }
-
-    /// <summary>
-    /// NavMesh上のポジションへ移動する
-    /// </summary>
-    /// <param name="pos">移動目標の座標</param>
-    protected void GoToTargetPos(Vector3 pos)
-    {
-
+        slow,
+        weakness,
     }
 }

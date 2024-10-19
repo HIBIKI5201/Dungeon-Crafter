0ba26ba0a63d153561e2b2f97bc2903eabcaa1ee
kobayashi koutarou <147145211+kobakoGlhf@users.noreply.github.com>
2024-10-19 13:30:07 +0900

    [update]SaveData(作業中)

--- Assets/Master/Script/System/Main/SaveDataManager.cs
+++ Assets/Master/Script/System/Main/SaveDataManager.cs
@@ -5,6 +5,7 @@ namespace DCFrameWork.MainSystem
     public class SaveDataManager : MonoBehaviour
     {
         private static GameSaveData _saveData = null;
+        static string _key = "TestKey";
         public static GameSaveData SaveData
         {
             get => _saveData;
@@ -19,20 +20,33 @@ namespace DCFrameWork.MainSystem
 
         public static void Save()
         {
-
+            var saveDataStr=JsonUtility.ToJson(SaveData);
+            var xorData = "";
+            for (int i = 0; i < saveDataStr.Length; i++)
+            {
+                xorData += (byte)saveDataStr[i]^(byte)_key[i%_key.Length-1];
+            }
+            PlayerPrefs.SetString("GameSaveData",saveDataStr);
         }
 
         public static void SettingSave()
         {
+            var settingSaveDataStr=JsonUtility.ToJson(SettingSaveData);
+            Debug.Log(settingSaveDataStr);
 
+            PlayerPrefs.SetString("SettingSaveData",settingSaveDataStr);
         }
 
         public static (GameSaveData, SettingSaveData) Load()
         {
-            return (null, null); //‰¼‚Ì–ß‚è’l
+            var gameSaveDataStr = PlayerPrefs.GetString("GemeSaveData");
+            var settingSaveDataStr = PlayerPrefs.GetString("SettintgSaveData");
+            GameSaveData gameSaveData = JsonUtility.FromJson<GameSaveData>(gameSaveDataStr);
+            SettingSaveData settingSaveData = JsonUtility.FromJson<SettingSaveData>(settingSaveDataStr);
+            return (gameSaveData, settingSaveData); //‰¼‚Ì–ß‚è’l
         }
     }
-
+    [System.Serializable]
     public class GameSaveData
     {
         public string SaveDate;
@@ -40,7 +54,7 @@ namespace DCFrameWork.MainSystem
         public List<int> PowerUpDatas;
         public int EventFlag;
     }
-
+    [System.Serializable]
     public class SettingSaveData
     {
         public int MasterVolume;

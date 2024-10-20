f3df179c9c6526a0db1f912c693aa954947b6dee
HIBIKI_5201 <takuto5201@gmail.com>
2024-10-21 01:46:36 +0900

    [update, fix]防衛設備のManagerにデータを受け取りコピーする機能を実装 これにより防衛設備のステータス種類を個別に設定可能に
    一部のサブベースクラスの継承先が間違っていた問題を修正

--- Assets/Master/Script/InGame/Tallet/DefenseEquipmentManager_B.cs
+++ Assets/Master/Script/InGame/Tallet/DefenseEquipmentManager_B.cs
@@ -2,24 +2,53 @@ using UnityEngine;
 
 namespace DCFrameWork.DefenseEquipment
 {
-    public abstract class DefenseEquipmentManager_B : MonoBehaviour
+    public abstract class DefenseEquipmentManager_B<Data> : MonoBehaviour where Data : DefenseEquipmentData_B
     {
         [SerializeField]
-        protected DefenseEquipmentData_B _data;
+        private DefenseEquipmentData_B _data;
 
         protected int Level;
+
+        #region 共通ステータス
+        protected float _attack;
+        protected float _rate;
+        protected float _range;
+        protected float _critical;
+        #endregion
 
         private void Start()
         {
             if (_data is null)
                 Debug.Log("データがありません");
+            LoadCommonData();
         }
+
+        private void LoadCommonData()
+        {
+            Data data = _data as Data;
+
+            _attack = data.Attack;
+            _rate = data.Rate;
+            _range = data.Range;
+            _critical = data.Critical;
+
+            LoadSpecificData(data);
+        }
+
+        /// <summary>
+        /// 設定した型パラメータに対応した専用変数を代入してください
+        /// </summary>
+        /// <param name="data">設備データ</param>
+        protected abstract void LoadSpecificData(Data data);
 
         private void Update()
         {
             Think();
         }
 
+        /// <summary>
+        /// Updateで実行されます
+        /// </summary>
         protected abstract void Think();
     }
 }

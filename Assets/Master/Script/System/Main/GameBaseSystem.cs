ad8c052ee5a6034e04c7cf169776e78e0a16413d
HIBIKI_5201 <takuto5201@gmail.com>
2024-10-22 02:33:41 +0900

    [update]MainSystemをGameBaseSystemに名称変更

--- Assets/Master/Script/System/Main/GameBaseSystem.cs
+++ Assets/Master/Script/System/Main/GameBaseSystem.cs
@@ -7,15 +7,15 @@ using UnityEngine.SceneManagement;
 
 namespace DCFrameWork.MainSystem
 {
-    public class MainSystem : MonoBehaviour
+    public class GameBaseSystem : MonoBehaviour
     {
         #region サービスロケーター
-        public static MainSystem mainSystem { get => _instance; }
+        public static GameBaseSystem mainSystem { get => _instance; }
         public static SceneSystem_B sceneSystem { get; private set; }
         #endregion
 
         #region メインシステム コンポーネント
-        private static MainSystem _instance;
+        private static GameBaseSystem _instance;
 
         private AudioManager _audioManager;
         private UIManager_B _mainUIManager;

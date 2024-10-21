ad3b08a4a5df022470a4e9902ba447794925d1cf
KasaharaSota <souta.programer1201@gmail.com>
2024-10-22 00:32:04 +0900

    [Update]オブジェクトを生成したタイミングでNavMeshを更新する処理の追加、Groundレイヤーのオブジェクトのみ取得するよう変更

--- Assets/Dev/Dev_Branchs/stage/GenarateObjectAlongGrid.cs
+++ Assets/Dev/Dev_Branchs/stage/GenarateObjectAlongGrid.cs
@@ -18,12 +18,13 @@ public class GenarateObjectAlongGrid : MonoBehaviour
     {
         _mainCamera = Camera.main;
         _prefabHeight = _prefab.GetComponent<BoxCollider>().size.y;
+        _navMeshSurface = FindAnyObjectByType<MockManager>().GetComponent<NavMeshSurface>();
     }
 
     void Update()
     {
         var ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
-        var raycastHitList = Physics.RaycastAll(ray).ToList();
+        var raycastHitList = Physics.RaycastAll(ray,float.PositiveInfinity,LayerMask.GetMask("Ground")).ToList();
         foreach(var raycastHit in raycastHitList)
         {
             Debug.Log(raycastHit.collider.gameObject.name);
@@ -33,8 +34,8 @@ public class GenarateObjectAlongGrid : MonoBehaviour
             _currentPosition = raycastHitList.OrderByDescending(x => x.collider.gameObject.transform.position.y).FirstOrDefault().point;
             //_currentPosition = raycastHitList.First().point;
             _clickPointPrefab.transform.position = _currentPosition;
+            
             _currentPosition.y = Mathf.Ceil(_currentPosition.y);
-            Debug.Log(_currentPosition);
             _currentPosition.y += _prefabHeight / 2;
             _currentPosition.x = (int)(_currentPosition.x) + 0.5f * Mathf.Sign(_currentPosition.x);
             _currentPosition.z = (int)(_currentPosition.z) + 0.5f * Mathf.Sign(_currentPosition.z);

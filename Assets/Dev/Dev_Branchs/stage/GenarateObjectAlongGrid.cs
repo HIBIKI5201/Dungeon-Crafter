a7829dfb0829cc7a18f47e9a81d72048bbceae8c
KasaharaSota <souta.programer1201@gmail.com>
2024-10-22 17:48:38 +0900

    [Update]オブジェクトをマウスクリックした面と隣接するグリッドに配置できるようにした

--- Assets/Dev/Dev_Branchs/stage/GenarateObjectAlongGrid.cs
+++ Assets/Dev/Dev_Branchs/stage/GenarateObjectAlongGrid.cs
@@ -9,7 +9,7 @@ public class GenarateObjectAlongGrid : MonoBehaviour
     [SerializeField] GameObject _prefab;
     [SerializeField] GameObject _clickPointPrefab;
     [SerializeField] GameObject _clickGridPrefab;
-    NavMeshSurface _navMeshSurface;
+    [SerializeField] NavMeshSurface _navMeshSurface;
     private Camera _mainCamera;
     private Vector3 _currentPosition = Vector3.zero;
     private float _prefabHeight;
@@ -18,32 +18,26 @@ public class GenarateObjectAlongGrid : MonoBehaviour
     {
         _mainCamera = Camera.main;
         _prefabHeight = _prefab.GetComponent<BoxCollider>().size.y;
-        _navMeshSurface = FindAnyObjectByType<MockManager>().GetComponent<NavMeshSurface>();
     }
 
     void Update()
     {
         var ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
-        var raycastHitList = Physics.RaycastAll(ray,float.PositiveInfinity,LayerMask.GetMask("Ground")).ToList();
-        foreach(var raycastHit in raycastHitList)
-        {
-            Debug.Log(raycastHit.collider.gameObject.name);
-        }
+        var raycastHitList = Physics.RaycastAll(ray, float.PositiveInfinity, LayerMask.GetMask("Ground")).ToList();
         if (raycastHitList.Any())
         {
-            _currentPosition = raycastHitList.OrderByDescending(x => x.collider.gameObject.transform.position.y).FirstOrDefault().point;
-            //_currentPosition = raycastHitList.First().point;
+            var hit = raycastHitList.OrderByDescending(x => x.collider.gameObject.transform.position.y).FirstOrDefault();
+            _currentPosition = hit.point;
             _clickPointPrefab.transform.position = _currentPosition;
-            
-            _currentPosition.y = Mathf.Ceil(_currentPosition.y);
-            _currentPosition.y += _prefabHeight / 2;
-            _currentPosition.x = (int)(_currentPosition.x) + 0.5f * Mathf.Sign(_currentPosition.x);
-            _currentPosition.z = (int)(_currentPosition.z) + 0.5f * Mathf.Sign(_currentPosition.z);
+            _currentPosition.y = (int)(_currentPosition.y + hit.normal.y / 2) + 0.5f;
+            _currentPosition.x = (int)(_currentPosition.x + hit.normal.x / 2) + 0.5f * Mathf.Sign(_currentPosition.x);
+            _currentPosition.z = (int)(_currentPosition.z + hit.normal.z / 2) + 0.5f * Mathf.Sign(_currentPosition.z);
             _clickGridPrefab.transform.position = _currentPosition;
         }
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(_prefab, _currentPosition, Quaternion.identity);
+            var obj = Instantiate(_prefab, _currentPosition, Quaternion.identity);
+            obj.transform.SetParent(_navMeshSurface.gameObject.transform);
             _navMeshSurface.BuildNavMesh();
         }
     }

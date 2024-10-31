b18034eeca4c8868ec8f2951238a275ab32e85ff
KasaharaSota <souta.programer1201@gmail.com>
2024-10-31 18:44:06 +0900

    [Update]5x5グリッド用に修正した

--- Assets/Dev/Dev_Branchs/stage/GenarateObjectAlongGrid.cs
+++ Assets/Dev/Dev_Branchs/stage/GenarateObjectAlongGrid.cs
@@ -38,12 +38,13 @@ public class GenarateObjectAlongGrid : MonoBehaviour
             var hit = raycastHitList.Where(x => x.collider.gameObject != _clickGridPrefab).OrderByDescending(x => x.collider.gameObject.transform.position.y).FirstOrDefault();
             _currentPosition = hit.point;
             _clickPointPrefab.transform.position = _currentPosition;
-            _currentPosition.y = (int)(_currentPosition.y + hit.normal.y / 2) + 0.5f;
-            _currentPosition.x = (int)(_currentPosition.x + hit.normal.x / 2) + 0.5f * Mathf.Sign(_currentPosition.x);
-            _currentPosition.z = (int)(_currentPosition.z + hit.normal.z / 2) + 0.5f * Mathf.Sign(_currentPosition.z);
+            //グリッドの計算式
+            _currentPosition.y = (int)((_currentPosition.y + hit.normal.y / 2) / 5) * 5 + 2.5f;
+            _currentPosition.x = (int)((_currentPosition.x + hit.normal.x / 2) / 5) * 5 + 2.5f * Mathf.Sign(_currentPosition.x);
+            _currentPosition.z = (int)((_currentPosition.z + hit.normal.z / 2) / 5) * 5 + 2.5f * Mathf.Sign(_currentPosition.z);
             _clickGridPrefab.transform.position = _currentPosition;
             Debug.DrawRay(_currentPosition, Vector3.down, Color.green, 1f);
-            if (_clickGridPrefab.transform.position.y > 2f || !Physics.Raycast(_currentPosition, Vector3.down, 1f, LayerMask.GetMask("Ground")))
+            if (_clickGridPrefab.transform.position.y > 8f || !Physics.Raycast(_currentPosition, Vector3.down, 5f, LayerMask.GetMask("Ground")))
             {
                 _canSet = false;
                 _clickGridPrefab.SetActive(false);
@@ -58,7 +59,7 @@ public class GenarateObjectAlongGrid : MonoBehaviour
         for (int i = 0; i < _path.corners.Length - 1; i++)
         {
             Debug.DrawLine(_path.corners[i], _path.corners[i + 1], Color.red);
-            if(Physics.Raycast(_path.corners[i], _path.corners[i + 1] - _path.corners[i], out RaycastHit hit, Vector3.Distance(_path.corners[i], _path.corners[i + 1]), LayerMask.GetMask("Ground")))
+            if (Physics.Raycast(_path.corners[i], _path.corners[i + 1] - _path.corners[i], out RaycastHit hit, Vector3.Distance(_path.corners[i], _path.corners[i + 1]), LayerMask.GetMask("Ground")))
             {
                 _clickGridPrefab.GetComponent<MeshRenderer>().material.color = Color.red;
                 _canSet = false;

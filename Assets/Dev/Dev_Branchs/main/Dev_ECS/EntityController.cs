2182400acc23d14d50b6154c3eb6fcad03d4fd55
HIBIKI_5201 <takuto5201@gmail.com>
2024-10-31 12:11:15 +0900

    [update]手動Agentを模索中

--- Assets/Dev/Dev_Branchs/main/Dev_ECS/EntityController.cs
+++ Assets/Dev/Dev_Branchs/main/Dev_ECS/EntityController.cs
@@ -8,34 +8,33 @@ namespace DCFrameWork
     public class EntityController : MonoBehaviour
     {
         [SerializeField]
-        Vector3 target = new Vector3(10, 0, -10);
+        Vector3 _target = new Vector3(10, 0, -10);
         NavMeshAgent _agent;
         [SerializeField]
         float _moveSpeed;
         Rigidbody _rigidbody;
+
+        [SerializeField]
+        bool _updatePos;
         void Start()
         {
             _rigidbody = GetComponent<Rigidbody>();
             _agent = GetComponent<NavMeshAgent>();
-            _agent.updatePosition = false;
-            _agent.SetDestination(target);
+            _agent.updatePosition = _updatePos;
+            _agent.SetDestination(_target);
         }
 
         private void Update()
         {
-            if (Input.GetKey(KeyCode.W))
-            {
-                ManualMove();
-            }
+            ManualMove();
         }
 
         private void ManualMove()
         {
             //経路が計算されているかを確認
             if (_agent.pathPending) return;
-
             // 位置の更新
-            transform.position += transform.forward * Time.deltaTime;
+            transform.position = _agent.nextPosition;
         }
     }
 }

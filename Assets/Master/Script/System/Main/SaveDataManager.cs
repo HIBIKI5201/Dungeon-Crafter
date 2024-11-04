f0b9132f3d9530673c511796adbbd42eff24a010
HIBIKI_5201 <takuto5201@gmail.com>
2024-11-04 16:28:29 +0900

    [update]データのロード時に自動でセーブデータインスタンスを更新するように

--- Assets/Master/Script/System/Main/SaveDataManager.cs
+++ Assets/Master/Script/System/Main/SaveDataManager.cs
@@ -12,22 +12,14 @@ namespace DCFrameWork.MainSystem
         public static GameSaveData SaveData
         {
             get => _saveData;
-            set
-            {
-                _saveData = value;
-                OnSaveDataChanged?.Invoke(value);
-            }
+            private set => _saveData = value;
         }
         private static SettingSaveData _settingSaveData = null;
         public static Action<SettingSaveData> OnSettingSaveDataChanged;
         public static SettingSaveData SettingSaveData
         {
             get => _settingSaveData;
-            set
-            {
-                _settingSaveData = value;
-                OnSettingSaveDataChanged?.Invoke(value);
-            }
+            private set => _settingSaveData = value;
         }
 
         public static void Save()
@@ -43,13 +35,18 @@ namespace DCFrameWork.MainSystem
 
         public static (GameSaveData, SettingSaveData) Load()
         {
-            return (JsonUtility.FromJson<GameSaveData>(Encryption(PlayerPrefs.GetString("GemeSaveData"))),
+            (GameSaveData data_g, SettingSaveData data_s) = (JsonUtility.FromJson<GameSaveData>(Encryption(PlayerPrefs.GetString("GemeSaveData"))),
                 JsonUtility.FromJson<SettingSaveData>(Encryption(PlayerPrefs.GetString("SettintgSaveData"))));
+            SaveData = data_g;
+            SettingSaveData = data_s;
+            return (data_g, data_s);
         }
+
         static string Encryption(string data) =>
             new string(data.Select(x => (char)(x ^ _key)).ToArray());
     }
-    [System.Serializable]
+
+    [Serializable]
     public class GameSaveData
     {
         public string SaveDate = "";
@@ -57,11 +54,12 @@ namespace DCFrameWork.MainSystem
         public List<int> PowerUpDatas = new List<int>();
         public int EventFlag = 0;
     }
-    [System.Serializable]
+    [Serializable]
     public class SettingSaveData
     {
         public int MasterVolume = 50;
         public int SoundEffectVolume = 50;
         public int BGMVolume = 50;
+        public int VoiceVolume = 50;
     }
 }

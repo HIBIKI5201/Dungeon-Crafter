33b22b7e922302b0e020a0c37acf7da40fa6a0cc
HIBIKI_5201 <takuto5201@gmail.com>
2024-11-04 11:03:29 +0900

    [fix]データベースがプロパティ表記によってシリアライズされなかった問題を修正

--- Assets/Master/Script/InGame/Tallet/DefenseEquipmentDataBase.cs
+++ Assets/Master/Script/InGame/Tallet/DefenseEquipmentDataBase.cs
@@ -6,9 +6,9 @@ namespace DCFrameWork.DefenseEquipment
 [CreateAssetMenu(menuName = "GameData/DefenseData/TurretDataBase", fileName = "DefebseEquipmentDataBase")]
     public class DefenseEquipmentDataBase : ScriptableObject
     {
-        public string Name { get; private set; }
-        public string Explanation { get; private set; }
-        public int DropChance { get; private set; }
-        public List<DefenseEquipmentData_B> DataLevelList { get; private set; }
+        public string Name;
+        public string Explanation;
+        public int DropChance;
+        public List<DefenseEquipmentData_B> DataLevelList;
     }
 }

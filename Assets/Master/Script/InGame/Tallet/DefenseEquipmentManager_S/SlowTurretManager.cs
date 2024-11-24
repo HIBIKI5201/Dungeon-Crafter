3a11ca882659e9a9b002ba7ac0557f498444c35b
NaritaShosei <onigirikun7171@gmail.com>
2024-11-25 04:33:42 +0900

    [fix]デバフ付与型の処理をBaseだけで完結するように

--- Assets/Master/Script/InGame/Tallet/DefenseEquipmentManager_S/SlowTurretManager.cs
+++ Assets/Master/Script/InGame/Tallet/DefenseEquipmentManager_S/SlowTurretManager.cs
@@ -5,42 +5,9 @@ namespace DCFrameWork.DefenseEquipment
 {
     public class SlowTurretManager : DEWalkerableManager_SB<DefenseEquipmentData_B>
     {
-        bool _isPaused = false;
         protected override void Think() //UpDate ‚Æ“¯‹`
         {
 
         }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            if (!_isPaused)
-            {
-                if (other.TryGetComponent(out IConditionable component))
-                {
-                    TargetAddCondition(component, ConditionType.slow);
-                }
-            }
-        }
-
-        private void OnTriggerExit(Collider other)
-        {
-            if (!_isPaused)
-            {
-                if (other.TryGetComponent(out IConditionable component))
-                {
-                    TargetRemoveCondition(component, ConditionType.slow);
-                }
-            }
-        }
-        protected override void Pause()
-        {
-            _isPaused = true;
-        }
-
-        protected override void Resume()
-        {
-            _isPaused = false;
-        }
-
     }
 }
--- Assets/Master/Script/InGame/Tallet/DefenseEquipmentManager_S/WeeknessTurretManager.cs
+++ Assets/Master/Script/InGame/Tallet/DefenseEquipmentManager_S/WeeknessTurretManager.cs
@@ -5,42 +5,9 @@ namespace DCFrameWork.DefenseEquipment
 {
     public class WeeknessTurretManager : DEWalkerableManager_SB<DefenseEquipmentData_B>
     {
-        bool _isPaused = false;
         protected override void Think() //UpDate ‚Æ“¯‹`
         {
 
         }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            if (!_isPaused)
-            {
-                if (other.TryGetComponent(out IConditionable component))
-                {
-                    TargetAddCondition(component, ConditionType.weakness);
-                }
-            }
-        }
-
-        private void OnTriggerExit(Collider other)
-        {
-            if (!_isPaused)
-            {
-                if (other.TryGetComponent(out IConditionable component))
-                {
-                    TargetRemoveCondition(component, ConditionType.weakness);
-                }
-            }
-        }
-        protected override void Pause()
-        {
-            _isPaused = true;
-        }
-
-        protected override void Resume()
-        {
-            _isPaused = false;
-        }
-
     }
 }

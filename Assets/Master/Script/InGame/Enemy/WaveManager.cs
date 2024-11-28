753d00e055469f0b6a0bb3f7d02596d7b13c2c15
kobakoGlhf <147145211+kobakoGlhf@users.noreply.github.com>
2024-11-29 00:47:22 +0900

    [update]EnemyGeneratorに値を入れる、_deathEnemyCountをstaticに

--- Assets/Master/Script/InGame/Enemy/WaveManager.cs
+++ Assets/Master/Script/InGame/Enemy/WaveManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using UnityEngine;
 
@@ -5,48 +6,54 @@ namespace DCFrameWork.Enemy
 {
     public class WaveManager : MonoBehaviour
     {
-        [SerializeField] EnemyGenerator[] _enemyGenerators;
-        [SerializeField] WaveData[] _waveData;
+        [SerializeField] EnemyGenerator _enemyGenerators;
+        [SerializeField] PhaseData _phaseData;
 
         int _waveCount;
         int _waveEnemySum;
-        int _deathEemyCount;
-        bool _isWave;
-        
+        static int _deathEemyCount;
+
+        public event Action _waveStartAction;
+        public event Action _waveEndAction;
+        public event Action _phaseEndAction;
+
         public int WaveCount { get => _waveCount; }
-        public bool IsWave { get => _isWave; }
         /// <summary>
         /// ウェーブの進行状況を正規化した値
         /// </summary>
         public float WaveProgressNormal { get => (float)_deathEemyCount / (_waveEnemySum != 0 ? _waveEnemySum : 1); }
-        WaveData NowWave { get => _waveData[_waveCount]; }
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        WaveData NowWave { get => _phaseData._waveData[_waveCount]; }
+
         private void Awake()
         {
-            //EnemyGeneratorに処理を追加
+            _waveStartAction += () => NextWave();
+            _waveStartAction?.Invoke();
         }
 
-        // Update is called once per frame
+
         void Update()
         {
             if (WaveProgressNormal == 1)
             {
-                Debug.Log("WaveEnd");
-                _isWave = false;
+                if (_waveCount != _phaseData._waveData.Length)
+                {
+                    _waveEndAction?.Invoke();
+                    _waveStartAction?.Invoke();
+                }
+                else
+                {
+                    _phaseEndAction?.Invoke();
+                }
             }
         }
 
         public void NextWave()
         {
-            if (_waveCount != _waveData.Length)
-            {
-                _deathEemyCount = 0;
-                _waveEnemySum = NowWave._spawnData.Sum(data => data._enemyCount);
-                _isWave = true;
-                _waveCount++;
-                Debug.Log("ウェーブ以降");
-            }
+            _deathEemyCount = 0;
+            _enemyGenerators._waveData = NowWave;
+            _waveEnemySum = NowWave._spawnData.Sum(data => data._enemyCount);
+            _waveCount++;
         }
-        public void EnemyDeathCount() => ++_deathEemyCount;//エネミー死亡時に呼んでほしい
+        public static void EnemyDeathCount() => ++_deathEemyCount;　//エネミー死亡時に呼んでほしい
     }
 }

5a9dd5bf22f720dd7bcd3257241b329c946d105a
mikenchi <mikenti337564@gmail.com>
2024-11-29 01:35:18 +0900

    [update]ステージセレクトマネージャーのロードシーンをゲームベースシステムを用いたものにしました。

--- Assets/Dev/Dev_Branchs/interface/StageSelectManager.cs
+++ Assets/Dev/Dev_Branchs/interface/StageSelectManager.cs
@@ -1,14 +1,20 @@
 using UnityEditor.Build.Content;
+using System;
 using UnityEngine;
 using DCFrameWork.MainSystem;
+using DCFrameWork.SceneSystem;
 
 namespace DCFrameWork
 {
-    public class StageSelectManager : MonoBehaviour
+    public class StageSelectManager 
     {
         public void StageSelect(SceneKind kind)
         {
-            SceneChanger.LoadScene(kind);
+            GameBaseSystem.mainSystem.LoadScene(kind);
+        }
+        public void StageSelect(SceneKind kind,StoryData storydata)
+        {
+            GameBaseSystem.mainSystem.LoadScene<StorySystem>(kind,x => x.SetStoryData(storydata));
         }
     }
 }

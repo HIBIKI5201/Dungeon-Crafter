9560e76963336774457b4e20dfab65bf8ffbc245
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2024-12-04 10:24:09 +0900

    [Add]leadenemy着手　instantiate時にHPバーがない状態

--- Assets/Master/Script/InGame/Enemy/EnemyManager_S/LeadEnemyManager.cs
+++ Assets/Master/Script/InGame/Enemy/EnemyManager_S/LeadEnemyManager.cs
@@ -1,7 +1,41 @@
+using DCFrameWork.MainSystem;
+using System.Collections;
+using UnityEngine;
+
 namespace DCFrameWork.Enemy
 {
     public class LeadEnemyManager : EnemyManager_B<EnemyData_B, EnemyRiseData>
     {
+
+        [SerializeField]
+        private GameObject _defenseEnemyPrefab;
+
+        private float _stoppMovingTime = 0.3f;
+        private float _coolTime = 1.5f;
+
+        [SerializeField]
+        private float _summonRange = 1;
+
+        private void Update()
+        {
+            InstantiateDefenseEnemy();
+        }
+
+        void InstantiateDefenseEnemy()
+        {
+            StartCoroutine(InstantiateEnemy());
+        }
+
+        IEnumerator InstantiateEnemy()
+        {
+            Instantiate(_defenseEnemyPrefab, gameObject.transform.forward.normalized * _summonRange, Quaternion.identity);
+            yield return FrameWork.PausableWaitForSecond(_coolTime);
+
+        }
+
+
+
+
         protected override void DeathBehaviour()
         {
             base.DeathBehaviour();

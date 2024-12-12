1c0b8b5bf12c058856c93740350c22cc3789a02d
mikenchi <mikenti337564@gmail.com>
2024-12-12 17:19:42 +0900

    [add]カスタムSliderを作成しました

--- Assets/Dev/Dev_Branchs/interface/Test.cs
+++ Assets/Dev/Dev_Branchs/interface/Test.cs
@@ -1,19 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 
-public class Test : VisualElement
+public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] UnityEvent uniev;
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("s"))
+        {
+            uniev.Invoke();
+        }
     }
 }

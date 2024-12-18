95fb72bad8d4277ecc9dd7985cf300551d16a18f
kobayashi koutarou <147145211+kobakoGlhf@users.noreply.github.com>
2024-12-18 20:31:19 +0900

    [add]DotCaling追加（作業中）

---
+++ Assets/Master/Script/InGame/DotCalling.cs
@@ -0,0 +1,24 @@
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+namespace DCFrameWork
+{
+    public class DotCalling : MonoBehaviour
+    {
+        NativeArray<float> result;
+        JobHandle jobHandle;
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}

78c1fb1b464d12e878b2eef2fc71fb52b25800d6
kobayashi koutarou <147145211+kobakoGlhf@users.noreply.github.com>
2024-12-20 22:14:43 +0900

    [update]DotCalling作成中

--- Assets/Master/Script/InGame/DotCalling.cs
+++ Assets/Master/Script/InGame/DotCalling.cs
@@ -1,5 +1,8 @@
+using System;
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Jobs;
 
@@ -7,18 +10,44 @@ namespace DCFrameWork
 {
     public class DotCalling : MonoBehaviour
     {
-        NativeArray<float> result;
-        JobHandle jobHandle;
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        DotJob _job=new();
+        MeshRenderer[] _mesh;
+        void Initialize()
         {
-        
+            var objects = GameObject.FindGameObjectsWithTag("DisappearOnBack");
+            _mesh = Array.ConvertAll(objects, x=>x.GetComponent<MeshRenderer>());
+            float3[] foward = Array.ConvertAll(_mesh,x=>new float3( x.transform.forward.x,x.transform.forward.y,x.transform.forward.z));
+            var nArray = new NativeArray<float3>(foward,Allocator.TempJob);
+            for(var i =0; i < _mesh.Length; i++)
+            {
+                nArray[i] = new float3(foward[i].x, foward[i].y, foward[i].z);
+            }
+            _job.objfoward = nArray;
         }
 
-        // Update is called once per frame
         void Update()
         {
-        
+            var a = _job.Schedule(_mesh.Length, 0);
+            a.Complete();
+            for (var i = 0; i < _mesh.Length; i++)
+            {
+                if (_job.result[i] < 0)
+                {
+                    _mesh[i].enabled = false;
+                }
+                else _mesh[i].enabled = true;
+            }
+        }
+    }
+    [BurstCompile]
+    struct DotJob : IJobParallelFor
+    {
+        public float3 cameraPos;
+        public NativeArray<float3> objfoward;
+        public NativeArray<float> result;
+        public void Execute(int index)
+        {
+            result[index] = math.dot(cameraPos, objfoward[index]);
         }
     }
 }

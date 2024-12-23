69f5a4d4d003332987c232c720baf82f7a4b9711
KasaharaSota <souta.programer1201@gmail.com>
2024-12-23 15:15:37 +0900

    [fix]StageManagerのtargetの座標をtransform型の変数で参照していたのをVector3に変更した。

--- Assets/Master/Script/InGame/Stage/StageManager.cs
+++ Assets/Master/Script/InGame/Stage/StageManager.cs
@@ -23,7 +23,7 @@ public class StageManager : MonoBehaviour
         [Tooltip("特注の視覚サポートオブジェクトがあればいれてください")] public GameObject VisualGuide;
     }
     Vector3[] _spawnPos;
-    Transform _targetPos;
+    Vector3 _targetPos;
     GameObject _setPrefab;
     GameObject _tentativePrefab;
     GameObject _wallsParent;
@@ -43,14 +43,14 @@ public class StageManager : MonoBehaviour
     {
         var enemyGenerator = GetComponentInChildren<EnemyGenerator>();
         _spawnPos = new Vector3[enemyGenerator.SpawnPos.Length];
-        _targetPos = enemyGenerator.TargetPos;
+        _targetPos = enemyGenerator.TargetPos.position;
         //スポーン地点と目標地点に障害物を置けないようにするための調整
         for (int i = 0; i < enemyGenerator.SpawnPos.Length; i++)
         {
             _spawnPos[i] = enemyGenerator.SpawnPos[i].position;
             _spawnPos[i] = new Vector3(_spawnPos[i].x, 2.5f, _spawnPos[i].z);
         }
-        _targetPos.position = new Vector3(_targetPos.position.x, 2.5f, _targetPos.position.z);
+        _targetPos = new Vector3(_targetPos.x, 2.5f, _targetPos.z);
         _wallsParent = new GameObject();
         _wallsParent.transform.SetParent(transform);
         _wallsParent.name = "Obstacle Parent";
@@ -63,8 +63,8 @@ public class StageManager : MonoBehaviour
         _sizeX = (int)(_floorPrefab.transform.localScale.x);
         _sizeZ = (int)(_floorPrefab.transform.localScale.z);
         _map = new int[_sizeX, _sizeZ];
-        _startX = (int)((_targetPos.position.x - _floorCenter.x + _floorPrefab.transform.localScale.x * _gridSize / 2 - _gridSize / 2) / _gridSize);
-        _startZ = (int)((_targetPos.position.z - _floorCenter.z + _floorPrefab.transform.localScale.z * _gridSize / 2 - _gridSize / 2) / _gridSize);
+        _startX = (int)((_targetPos.x - _floorCenter.x + _floorPrefab.transform.localScale.x * _gridSize / 2 - _gridSize / 2) / _gridSize);
+        _startZ = (int)((_targetPos.z - _floorCenter.z + _floorPrefab.transform.localScale.z * _gridSize / 2 - _gridSize / 2) / _gridSize);
         //デフォルトで配置するオブジェクトをセットしてる。後から消すかも？
         _setPrefab = _obstaclePrefabList[0].PutObstaclePrefab;
         if (_obstaclePrefabList[0].VisualGuide == null)
@@ -145,7 +145,7 @@ public class StageManager : MonoBehaviour
             {
                 _tentativePrefab.SetActive(true);
                 //置けるかどうかの判定
-                if (CheckStage(_currentPosition) && !_spawnPos.Contains(_currentPosition) && _currentPosition != _targetPos.position)
+                if (CheckStage(_currentPosition) && !_spawnPos.Contains(_currentPosition) && _currentPosition != _targetPos)
                 {
                     _tentativePrefab.GetComponent<MeshRenderer>().material.color = Color.white;
                     _canSet = true;

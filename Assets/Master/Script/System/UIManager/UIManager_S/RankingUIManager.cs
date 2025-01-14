76d194186af7959d9f569474d6fd75b8ad377b6b
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-01-14 14:49:41 +0900

    [Add]Rankingをシングルトンに　rannking処理実装

--- Assets/Master/Script/System/UIManager/UIManager_S/RankingUIManager.cs
+++ Assets/Master/Script/System/UIManager/UIManager_S/RankingUIManager.cs
@@ -1,16 +1,51 @@
 using DCFrameWork.UI;
+using System;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace DCFrameWork.UI
 {
-    public class RankingUIManager : UIManager_B
+    public class RankingUIManager : MonoBehaviour 
     {
-
-        protected override Task LoadDocumentElement(VisualElement root)
+        public static RankingUIManager instance;
+        Label[] _labels = new Label[3];
+        UIDocument _uiDocument;
+        private int[] _scores = new int[4];
+        private void Start()
         {
-            throw new System.NotImplementedException();
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
+            _uiDocument = GetComponent<UIDocument>();
+
+            for(int i = 0; i < _labels.Length; i++)
+            {
+                _labels[i] = _uiDocument.rootVisualElement.Q<Label>($"Ranktext{i + 1}");
+            }
+
+            foreach(Label label in _labels)
+            {
+                label.text = "0";
+            }
+        }
+
+        public void UpdateRanking(int score)
+        {
+            _scores[3] = score;
+            Array.Sort( _scores );
+            Array.Reverse( _scores );
+            for(int i = 0; i < _scores.Length - 1; i++)
+            {
+                _labels[i].text = _scores[i].ToString();
+            }
         }
 
     }

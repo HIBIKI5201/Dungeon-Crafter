bf141061f1ac84d951a03b42b99761322666e069
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-01-14 12:50:58 +0900

    [Add]RankingUImanager着手ui細部の調整

---
+++ Assets/Master/Script/System/UIManager/UIManager_S/RankingUIManager.cs
@@ -0,0 +1,17 @@
+using DCFrameWork.UI;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace DCFrameWork.UI
+{
+    public class RankingUIManager : UIManager_B
+    {
+
+        protected override Task LoadDocumentElement(VisualElement root)
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}

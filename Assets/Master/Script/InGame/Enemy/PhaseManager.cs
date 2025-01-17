30b8b1209fc59fd2ede5815fc897f8e568eee33a
kobayashi koutarou <147145211+kobakoGlhf@users.noreply.github.com>
2025-01-17 17:08:16 +0900

    [update]命名の修正、エネミーが出ない問題の修正

--- Assets/Master/Script/InGame/Enemy/PhaseManager.cs
+++ Assets/Master/Script/InGame/Enemy/PhaseManager.cs
@@ -10,7 +10,8 @@ namespace DCFrameWork.Enemy
     {
         [SerializeField] EnemyGenerator _enemyGenerators;
         [SerializeField] EnemyPhaseData _phaseData;
-        [SerializeField] float _phaseWaitingTime = 20;
+        [SerializeField] float _startWaitingTimer = 10;
+        [SerializeField] float _phaseWaitingTime = 5;
 
         int _phaseCount;
         int _loopCount = 1;
@@ -19,35 +20,41 @@ namespace DCFrameWork.Enemy
 
         public event Action _phaseStartAction;
         public event Action _phaseEndAction;
+
+        /// <summary>
+        /// 引数1：正規化されたPhaseの
+        /// </summary>
         public event Action<float> PhaseProgressChanged;
 
-        public int WaveCount { get => _phaseCount; private set => _phaseCount = value; }
-        int CurrentWaveIndex { get => _phaseCount % _phaseData.WaveData.Length; }
+        public int PhaseCount { get => _phaseCount; private set => _phaseCount = value; }
+        int CurrentPhaseIndex { get => _phaseCount % _phaseData.PhaseData.Length; }
         /// <summary>
         /// ウェーブの進行状況を正規化した値
         /// </summary>
-        public float WaveProgressNormalize { get => (float)_deathEemyCount / (_phaseEnemySum != 0 ? _phaseEnemySum : 1); }
+        public float PhaseProgressNormalize { get => (float)_deathEemyCount / (_phaseEnemySum != 0 ? _phaseEnemySum : 1); }
 
-        public void Initialize()
+        public async void Initialize()
         {
             if (!_phaseData)
             {
                 Debug.Log("PhaseData is null");
                 return;
             }
-            _phaseStartAction += WaveEndCheck;
-            _phaseStartAction += () => NextWave();
+            PhaseProgressChanged += x => PhaseEndCheck();
+            _phaseStartAction += () => NextPhase();
+            await FrameWork.PausableWaitForSecondAsync(_startWaitingTimer);
             _phaseStartAction?.Invoke();
+            PhaseEndCheck();
         }
 
-        async void WaveEndCheck()
+        async void PhaseEndCheck()
         {
-            if (WaveProgressNormalize >= 1)
+            if (PhaseProgressNormalize >= 1)
             {
-                WaveCount++;
-                if (CurrentWaveIndex == 0 && WaveCount != 0)
+                PhaseCount++;
+                if (CurrentPhaseIndex == 0 && PhaseCount != 0)
                 {
-                    Debug.Log("pheseEnd" + $"WaveCount:{WaveCount}");
+                    Debug.Log("pheseEnd " + $"WaveCount:{PhaseCount}");
                     _loopCount++;
                 }
                 _phaseEndAction?.Invoke();
@@ -56,24 +63,23 @@ namespace DCFrameWork.Enemy
             }
         }
 
-        void NextWave()
+        void NextPhase()
         {
             _deathEemyCount = 0;
 
             //選択肢の中からランダムなデータを取得
-            var waveData = _phaseData.WaveData[CurrentWaveIndex].
-                SelectintWaveData[Random.Range(0, _phaseData.WaveData.Length)];
+            var phaseData = _phaseData.PhaseData[CurrentPhaseIndex].
+                SelectintPhaseData[Random.Range(0, _phaseData.PhaseData.Length)];
 
-            waveData.SpawnData.Select(x => x._enemyLevel += _loopCount - 1);//周回ごとのレベル上昇
-            Debug.Log(_loopCount - 1);
-            _phaseEnemySum = waveData.SpawnData.Length;
+            phaseData.SpawnData.Select(x => x._enemyLevel += _loopCount - 1);//周回ごとのレベル上昇
+            _phaseEnemySum = phaseData.SpawnData.Length;
 
-            _enemyGenerators.Waving(waveData);
+            _enemyGenerators.Waving(phaseData);
         }
         public void EnemyDeathCount()　//エネミー死亡時に呼んでほしい
         {
             ++_deathEemyCount;
-            PhaseProgressChanged?.Invoke(WaveProgressNormalize);
+            PhaseProgressChanged?.Invoke(PhaseProgressNormalize);
         }
 
     }

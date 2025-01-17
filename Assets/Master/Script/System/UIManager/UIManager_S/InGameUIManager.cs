ab0050d17a7f373386234336a19e8d98ebb3df5d
mikenchi <mikenti337564@gmail.com>
2025-01-17 18:37:50 +0900

    [update]InGameUIManagerにUIの上にマウスがあるかどうか判別するためのeventのついか

--- Assets/Master/Script/System/UIManager/UIManager_S/InGameUIManager.cs
+++ Assets/Master/Script/System/UIManager/UIManager_S/InGameUIManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Threading.Tasks;
 using DCFrameWork.DefenseEquipment;
+using DCFrameWork.Enemy;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -9,9 +11,11 @@ namespace DCFrameWork.UI
     public class InGameUIManager : UIManager_B
     {
         [SerializeField] StageManager _stageManager;
+        [SerializeField] PhaseManager _phaseManager;
         EquipmentCardInventory _equipmentList;
         BasicInformation _basicInformation;
         EquipmentSettingUI _equipmentSettingUI;
+        public event Action<bool> OnMouseOnUI;
         protected override async Task LoadDocumentElement(VisualElement root)
         {
             _equipmentList = root.Q<EquipmentCardInventory>("EquipmentCardInventory");
@@ -20,10 +24,22 @@ namespace DCFrameWork.UI
             await _equipmentList.InitializeTask;
             await _basicInformation.InitializeTask;
             await _equipmentSettingUI.InitializeTask;
+            _stageManager.OnActivateTurretSelectedUI += EquipmentSettingUIUpdate;
+            _phaseManager.PhaseProgressChanged += PhaseUpdate;
+            _phaseManager._phaseEndAction += PhaseCount;
         }
         void EquipmentSettingUIUpdate(ITurret turret)
         {
-            
+            _equipmentSettingUI.EquipmentSettingWindowVisible = true;
+        }
+        void PhaseUpdate(float parsent)
+        {
+            _basicInformation.GuageMesh.UpdateGuage(parsent * 100);
+        }
+        void PhaseCount()
+        {
+            _basicInformation.PhaseText = _phaseManager.PhaseCount.ToString();
+            _basicInformation.GuageMesh.UpdateGuage(0);
         }
     }
 }

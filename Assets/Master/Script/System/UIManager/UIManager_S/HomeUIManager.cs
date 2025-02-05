a6d8b5f4bce4feb69167d01e8547d50124779ed2
Oriku0308 <O.riku8863@gmail.com>
2025-02-06 04:21:11 +0900

    <Add> タイトルからメインメニューへ遷移して、そのなかのステージボタンを押すとインゲームに遷移するシステムを再実装 メインメニューからタイトルに戻る機能は、それ用のボタンのUXMLがボタンとして作られていなかったため断念

--- Assets/Master/Script/System/UIManager/UIManager_S/HomeUIManager.cs
+++ Assets/Master/Script/System/UIManager/UIManager_S/HomeUIManager.cs
@@ -8,16 +8,19 @@ namespace DCFrameWork.UI
 {
     public class HomeUIManager : UIManager_B
     {
-        TemplateContainer _title;
-        Button _startButton;
-        Button _creditButton;
+        private TitleWindow _titleWindow;
+        private HomeWindow _homeWindow;
+
+
         protected override async Task LoadDocumentElement(VisualElement root)
         {
             await Task.CompletedTask;
-            _title = root.Q<TemplateContainer>("Title");
-            _startButton = root.Q<Button>("GameStartButton");
-            _creditButton = root.Q<Button>("CreditButton");
-            _startButton.clicked += () => GameBaseSystem.mainSystem.LoadScene(SceneKind.Ingame_1);
+            _titleWindow  = root.Q<TitleWindow>();
+            _homeWindow = root.Q<HomeWindow>();
+            _titleWindow.GameStartButton.clicked += () => _titleWindow.style.visibility = Visibility.Hidden;
+            //_homeWindow.TitleButton.clicked += () => _titleWindow.style.visibility = Visibility.Visible;
+            _homeWindow.StageButton.clicked += () => GameBaseSystem.mainSystem.LoadScene(SceneKind.Ingame_1);
+            //_startButton.clicked += () => GameBaseSystem.mainSystem.LoadScene(SceneKind.Ingame_1);
         }
     }
 }
